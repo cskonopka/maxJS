77cf8291d509a208af696d1f072f2952db173786
Christopher Konopka <cskonopka@gmail.com>
2015-03-26 01:01:46 -0400

    slimming folder

--- connect2objects.js
+++
@@ -1,7 +0,0 @@
-function bang()
-{
-	_p = this.patcher
-	tog1 = _p.newobject("toggle", 125, 20, 40, 10);
-	tog2 = _p.newobject("toggle", 125, 125, 40, 10);	
-	_p.connect(tog1, 0, tog2, 0);
-}
--- createToggle.js
+++
@@ -1,4 +0,0 @@
-function bang()
-{
-	this.patcher.newobject("toggle", 125, 20, 40, 10);
-}
